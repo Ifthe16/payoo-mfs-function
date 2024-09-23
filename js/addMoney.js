5f08379bb76cfd4eca97594e371350c23c12c24b
Ifthe16 <habiburrahmanif975@gmail.com>
2024-09-23 17:55:20 +0600

    created utilties file with function name

---
+++ js/addMoney.js
@@ -0,0 +1,17 @@
+/**
+ * How to get a number from input field
+ * 1. create a variable
+ * 2. right side document.getElementById(id)
+ * 3. .value
+ * 4. parseFloat
+*/
+
+console.log('add money loaded');
+document.getElementById('btn-add-money')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+
+        console.log('add money button clicked');
+        // const addMoney = document.getElementById('input-add-money').value;
+        // const addMoneyNumber = parseFloat(addMoney);
+    });
---
+++ js/utilities.js
@@ -0,0 +1,9 @@
+/** 
+ * Common Shared function here
+*/
+console.log('utilities loaded');
+
+
+function getInputFieldValueById() {
+    console.log('will get value by id');
+}
